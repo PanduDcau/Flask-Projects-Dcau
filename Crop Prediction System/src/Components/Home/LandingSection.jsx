adf6c4396ece1aaca298d97b12c76b0147ea5dd8
W. Pandula <52339029+PanduDcau@users.noreply.github.com>
2023-03-06 23:37:09 +0530

    Landing

---
+++ Crop Prediction System/src/Components/Home/LandingSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './LandingSection.scss'
+
+function LandingSection() {
+    return (
+        <div className="section landingSection">
+            <h1>Agricultural</h1>
+            <h2>Crop and Fertilizer</h2>
+            <h2>Recommendation.</h2>
+            <p>Using Data Science and Machine Learning Approaches</p>
+        </div>
+    )
+}
+
+export default LandingSection
